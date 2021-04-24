fc58dd190ed9f4c4ddc69ba3440872ad4dc9515a
Ricky Burgin <ricky@burg.in>
2021-04-24 23:37:04 +0900

    Preventing mounted data from being chowned, for use with Unraid server

--- 2.4/docker-entrypoint.sh
+++ 2.4/docker-entrypoint.sh
@@ -101,6 +101,8 @@ fi
 # Create directories for Dav data and lock database.
 [ ! -d "/var/lib/dav/data" ] && mkdir -p "/var/lib/dav/data"
 [ ! -e "/var/lib/dav/DavLock" ] && touch "/var/lib/dav/DavLock"
-chown -R www-data:www-data "/var/lib/dav"
+# chown -R www-data:www-data "/var/lib/dav"
+# Only chowning Davlock for use with Unraid
+chown www-data:www-data "/var/lib/dav/DavLock"
 
 exec "$@"
